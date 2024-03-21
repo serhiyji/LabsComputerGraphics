8fcffc3815b587d057f8bb575e827c4f66e4a212
SerhiyJI <serhiyji@gmail.com>
2024-03-21 14:54:39 +0200

    Add lab8

---
+++ Lab8/FractalSnowflakeKohi.cs
@@ -0,0 +1,41 @@
+﻿using Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8
+{
+    public class FractalSnowflakeKohi
+    {
+        public static List<MyPoint> GetRightTriangle(MyPoint center, double length) => new() {
+                (center.x - length / 2, center.y + Math.Sqrt(3) * length / 6),
+                (center.x + length / 2, center.y + Math.Sqrt(3) * length / 6),
+                (center.x, center.y - Math.Sqrt(3) * length / 3)
+            };
+        
+        public static List<MyPoint> GenerateKochSnowflake(MyPoint center, int iterations, double length)
+        {
+            List<MyPoint> points = GetRightTriangle(center, length);
+            for (int i = 0; i < iterations; i++)
+            {
+                points = NextIteration(points);
+            }
+            return points;
+        }
+        public static List<MyPoint> NextIteration(List<MyPoint> points)
+        {
+            List<MyPoint> newPoints = new List<MyPoint>();
+            for (int j = 0; j < points.Count; j++)
+            {
+                MyPoint p1 = points[j], p2 = points[(j + 1) % points.Count];
+                newPoints.Add(p1);
+                newPoints.Add(new MyPoint((2 * p1.x + p2.x) / 3, (2 * p1.y + p2.y) / 3));
+                newPoints.Add(new MyPoint((p1.x + p2.x) / 2 - Math.Sqrt(3) / 6 * (p2.y - p1.y), (p1.y + p2.y) / 2 + Math.Sqrt(3) / 6 * (p2.x - p1.x)));
+                newPoints.Add(new MyPoint((p1.x + 2 * p2.x) / 3, (p1.y + 2 * p2.y) / 3));
+            }
+            return newPoints;
+        }
+    }
+}
---
+++ Lab8/Program.cs
@@ -0,0 +1,80 @@
+﻿using Extensions;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using System.Collections;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace Lab8
+{
+    public class WindowLab8 : BaseWindow
+    {
+        List<MyPoint> points;
+        List<List<MyPoint>> data = new List<List<MyPoint>>();
+        int min = 0, max = 10, current = 0, rendermax = 0;
+        //Counter counter = new Counter(1, 6, 1, 1, 0, true);
+        public WindowLab8() : base(BaseWindow.DefaultSetupWindow()) 
+        {
+            data.Add(FractalSnowflakeKohi.GetRightTriangle((0, 0), 1.5));
+            //for (int i = 0; i < 6; i++)
+            //{
+            //    data.Add(FractalSnowflakeKohi.NextIteration(data.LastOrDefault()));
+            //}
+            points = data[0];
+        }
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            //points = data[(int)counter.Next];
+            Painter.LineLoopV3(points);
+            SwapBuffers();
+            base.OnRenderFrame(args);
+        }
+
+        protected override void OnKeyDown(KeyboardKeyEventArgs e)
+        {
+            base.OnKeyDown(e);
+            switch (e.Key)
+            {
+                case OpenTK.Windowing.GraphicsLibraryFramework.Keys.Up:
+                    if(current + 1 <= rendermax)
+                    {
+                        current++;
+                        points = data[current];
+                    }
+                    else if (current == rendermax && current != max)
+                    {
+                        DateTime start = DateTime.UtcNow;
+                        data.Add(FractalSnowflakeKohi.NextIteration(data[current]));
+                        DateTime end = DateTime.UtcNow;
+                        Console.WriteLine($"{current} -> {current + 1} {end - start}");
+                        current++;
+                        rendermax++;
+                        points = data[current];
+                    }
+                    break;
+                case OpenTK.Windowing.GraphicsLibraryFramework.Keys.Down:
+                    if (current != min)
+                    {
+                        current -= 1;
+                        points = data[current];
+                    }
+                    break;
+            }
+            double sizeInBytes = System.GC.GetTotalMemory(true);
+            double sizeInMegabytes = sizeInBytes / (1024 * 1024);
+            Title = $"{current} {sizeInMegabytes}";
+        }
+    }
+    public static class Program
+    {
+        public static void Main()
+        {
+            using (WindowLab8 windowLab1 = new WindowLab8())
+            {
+                windowLab1.Run();
+            }
+        }
+    }
+}
